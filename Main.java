07b18717ec3aabbea8b12d6bd6fb8994cdc0e863
choijiho0515 <163032555+choijiho0515@users.noreply.github.com>
2025-06-16 21:08:29 +0900

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,33 +1,14 @@
-interface Animal {
-    void makeSound();
-}
-
-class Dog implements Animal {
-    @Override
-    public void makeSound() {
-        System.out.println("멍멍!");
-    }
-
-    public static void makeLoud() {
-        System.out.println("으르르깡깡!");
-    }
-}
-public class  Main {
-    public static void main(String[] Args) {
-
-        Animal dog = new Dog();
-
-        dog.makeSound();
-
-        if (dog instanceof Dog) {
-            Dog newDog = new  Dog();
-
-            newDog.makeLoud();
+class Outer {
+    static class StaticInner {
+        void hello() {
+            System.out.println("Hi");
         }
-
     }
 }
 
-
-
-
+public class Main {
+    public static void main(String[] args) {
+        Outer.StaticInner inner = new Outer.StaticInner();
+        inner.hello();
+    }
+}
