4ead9cd82a1093d87a210a6c8de017ee3741ca58
choijiho0515 <163032555+choijiho0515@users.noreply.github.com>
2025-06-20 21:17:11 +0900

    Block Chain

---
+++ Block.java
@@ -0,0 +1,29 @@
+package org.example;
+
+public class Block {
+    public String hash;
+    public String previousHash;
+    private String data;            // 오리지날: Transaction
+    private long timeStamp;
+    private int nonce = 0;          // 정해진 값이 있음
+
+    public Block(String data, String previousHash) {
+        this.data = data;
+        this.previousHash = previousHash;
+        this.timeStamp = System.currentTimeMillis();
+    }
+
+    public String calculateHash() {
+        String input = previousHash + timeStamp + data + nonce;
+
+        return StringUtil.applySha256(input);
+    }
+    public void mineBlock(int difficulty) {
+        String target = new String(new char[difficulty]).replace('\0', '0');
+        do {
+            nonce++;
+            hash = calculateHash();
+        } while  (!hash.substring(0, difficulty).equals(target));
+
+    }
+}
